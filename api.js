07b4a57f92cc8cf18aa7693b3421b967090c0fd7
Giovanni Sampaio <giovannibruno1@gmail.com>
2017-07-03 14:30:50 -0300

    Include Product endpoints in the API

--- api.js
+++ api.js
@@ -1,42 +1,62 @@
+var bodyparser = require('body-parser');
 var express = require('express');
 var status = require('http-status');
 
 module.exports = function(wagner) {
   var api = express.Router();
 
-  api.get('/category/id/:id', wagner.invoke(function(Category) {
+  api.use(bodyparser.json());
+
+  api.put('/me/cart', wagner.invoke(function(User) {
     return function(req, res) {
-      Category.findOne({ _id: req.params.id }, function(error, category) {
+      try {
+        var cart = req.body.data.cart;
+      } catch(e) {
+        return res.
+          status(status.BAD_REQUEST).
+          json({ error: 'No cart specified!' });
+      }
+
+      req.user.data.cart = cart;
+      req.user.save(function(error, user) {
         if (error) {
           return res.
             status(status.INTERNAL_SERVER_ERROR).
             json({ error: error.toString() });
         }
-        if (!category) {
-          return res.
-            status(status.NOT_FOUND).
-            json({ error: 'Not found' });
-        }
-        res.json({ category: category });
+        return res.json({ user: user });
       });
     };
   }));
 
-  api.get('/category/parent/:id', wagner.invoke(function(Category) {
-    return function(req, res) {
-      Category.
-        find({ parent: req.params.id }).
-        sort({ _id: 1 }).
-        exec(function(error, categories) {
-          if (error) {
-            return res.
-              status(status.INTERNAL_SERVER_ERROR).
-              json({ error: error.toString() });
-          }
-          res.json({ categories: categories });
-        });
-    };
-  }));
+  api.get('/me', function(req, res) {
+    if (!req.user) {
+      return res.
+        status(status.UNAUTHORIZED).
+        json({ error: 'Not logged in' });
+    }
+
+    req.user.populate(
+      { path: 'data.cart.product', model: 'Product' },
+      handleOne.bind(null, 'user', res));
+  });
 
   return api;
 };
+
+function handleOne(property, res, error, result) {
+  if (error) {
+    return res.
+      status(status.INTERNAL_SERVER_ERROR).
+      json({ error: error.toString() });
+  }
+  if (!result) {
+    return res.
+      status(status.NOT_FOUND).
+      json({ error: 'Not found' });
+  }
+
+  var json = {};
+  json[property] = result;
+  res.json(json);
+}
--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 // Set up Mongoose and the Category model
 var mongoose = require('mongoose');
+var _ = require('underscore');
 
 // Connects to MongoDB
 module.exports = function(wagner) {
@@ -8,11 +9,23 @@ module.exports = function(wagner) {
   // Creates a Mongoose model by including the schema
   var Category = 
     mongoose.model('Category', require('./category'), 'categories');
+  var Product = 
+    mongoose.model('Product', require('./product'), 'products');
   
+  var models = {
+    Category: Category,
+    Product: Product
+  };
+
+  // To ensure DRY-ness, register factories in a loop
+  _.each(models, function(value, key) {
+    wagner.factory(key,function() {
+      return value;
+    });
   // Register the Category service with Wagner
-  wagner.factory('Category', function() {
-    return Category;
-  });
+  // wagner.factory('Category', function() {
+  //   return Category;
+  // });
   
   return {
     Category: Category
