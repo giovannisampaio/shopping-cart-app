f5b91e7ead5b7f7b2ac005428d25da1d95032d42
Giovanni Sampaio <giovannibruno1@gmail.com>
2017-07-04 09:40:40 -0300

    Add Product model

--- models.js
+++ models.js
@@ -1,33 +1,28 @@
-// Set up Mongoose and the Category model
 var mongoose = require('mongoose');
 var _ = require('underscore');
 
-// Connects to MongoDB
 module.exports = function(wagner) {
   mongoose.connect('mongodb://localhost:27017/test');
 
-  // Creates a Mongoose model by including the schema
-  var Category = 
+  var Category =
     mongoose.model('Category', require('./category'), 'categories');
-  var Product = 
+  var Product =
     mongoose.model('Product', require('./product'), 'products');
-  
+  var User =
+    mongoose.model('User', require('./user'), 'users');
+
   var models = {
     Category: Category,
-    Product: Product
+    Product: Product,
+    User: User
   };
 
   // To ensure DRY-ness, register factories in a loop
   _.each(models, function(value, key) {
-    wagner.factory(key,function() {
+    wagner.factory(key, function() {
       return value;
     });
-  // Register the Category service with Wagner
-  // wagner.factory('Category', function() {
-  //   return Category;
-  // });
-  
-  return {
-    Category: Category
-  };
+  });
+
+  return models;
 };
