7897ecdd9d0c71b0b776e878e5bd7c3f441ce1a7
Giovanni Sampaio <giovannibruno1@gmail.com>
2017-07-05 18:59:43 -0300

    Add Facebook autentication login

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var wagner = require('wagner-core');
 
-// Bootstrap the Mongoose models using the models.js file
 require('./models')(wagner);
 
 var app = express();
 
+wagner.invoke(require('./auth'), { app: app });
 
-// Bootstrap the application using the api.js file
-// api.js will return an Express subrouter
-// Express app will redirect to this router everytime URL starts with /api/v1
 app.use('/api/v1', require('./api')(wagner));
 
-// Start the HTTP server on port 3000
 app.listen(3000);
 console.log('Listening on port 3000!');
