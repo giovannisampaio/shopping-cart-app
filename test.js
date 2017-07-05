9f1bfe11fa9a4162c71929d4bd12ed44748f48bd
Giovanni Sampaio <giovannibruno1@gmail.com>
2017-07-05 20:42:48 -0300

    Install Stripe to handle credit card charges

--- test.js
+++ test.js
@@ -7,21 +7,25 @@ var wagner = require('wagner-core');
 var URL_ROOT = 'http://localhost:3000';
 var PRODUCT_ID = '000000000000000000000001';
 
-describe('User API', function() {
+describe('User Checkout', function() {
   var server;
   var Category;
   var Product;
+  var Stripe;
   var User;
 
+  this.timeout(15000);	
   before(function() {
     var app = express();
 
     // Bootstrap server
     models = require('./models')(wagner);
+    dependencies = require('./dependencies')(wagner);
 
     // Make models available in tests
     Category = models.Category;
     Product = models.Product;
+    Stripe = dependencies.Stripe;
     User = models.User;
 
     app.use(function(req, res, next) {
@@ -159,6 +163,54 @@ describe('User API', function() {
           assert.equal(result.data.cart[0].quantity, 1);
           done();
         });
+      });
+    });
+  });
+
+  it('can check out', function(done) {
+    var url = URL_ROOT + '/checkout';
+
+    // Set up data
+    User.findOne({}, function(error, user) {
+      assert.ifError(error);
+      user.data.cart = [{ product: PRODUCT_ID, quantity: 1 }];
+      user.save(function(error) {
+        assert.ifError(error);
+
+        // Attempt to check out by posting to /api/v1/checkout
+        superagent.
+          post(url).
+          send({
+            // Fake stripe credentials. stripeToken can either be
+            // real credit card credentials or an encrypted token -
+            // in production it will be an encrypted token.
+            stripeToken: {
+              number: '4242424242424242',
+              cvc: '123',
+              exp_month: '12',
+              exp_year: '2018'
+            }
+          }).
+          end(function(error, res) {
+            assert.ifError(error);
+
+            assert.equal(res.status, 200);
+            var result;
+            assert.doesNotThrow(function() {
+              result = JSON.parse(res.text);
+            });
+
+            // API call gives us back a charge id.
+            assert.ok(result.id);
+
+            // Make sure stripe got the id
+            Stripe.charges.retrieve(result.id, function(error, charge) {
+              assert.ifError(error);
+              assert.ok(charge);
+              assert.equal(charge.amount, 2000 * 100); // 2000 USD
+              done();
+            });
+          });
       });
     });
   });
